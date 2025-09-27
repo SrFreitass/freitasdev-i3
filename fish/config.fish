255b44648b32729da9d47b35792e728f63f0f7b6
SrFreitass <guilhermefreitasdonascimento67@gmail.com>
2025-09-27 11:14:38 -0400

    feat: add oh-my-fish and fish config

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+alias l=ls
+alias c=clear
+alias cat=bat
+mise activate fish | source
